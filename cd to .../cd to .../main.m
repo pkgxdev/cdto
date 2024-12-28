24d00dcd4559f97db0ae6e3bb80b07ac0c2cc579
Max Howell <mxcl@me.com>
2024-12-28 07:39:30 -0500

    Don’t use `/usr/bin/open`

--- cd to .../cd to .../main.m
+++ cd to .../cd to .../main.m
@@ -20,14 +20,14 @@ NSUInteger linesOfHistory(TerminalTab* tab) {
 NSString* getBundleIDForUTI(NSString* uti) {
     CFStringRef cfUTI = (__bridge CFStringRef)uti;
     LSRolesMask role = kLSRolesAll;
-    
+
     CFStringRef bundleID = LSCopyDefaultRoleHandlerForContentType(cfUTI, role);
-    
+
     if (bundleID != NULL) {
         NSString* result = (__bridge_transfer NSString*)bundleID;
         return result;
     }
-    
+
     return nil;
 }
 
@@ -35,10 +35,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
         FinderApplication* finder = [SBApplication applicationWithBundleIdentifier:@"com.apple.Finder"];
-        
+
         NSString* uti = @"public.unix-executable";
         NSString* bundleID = getBundleIDForUTI(uti);
-        
+
         FinderItem *target = [(NSArray*)[[finder selection] get] firstObject];
         FinderFinderWindow* findWin = [[finder FinderWindows] objectAtLocation:@1];
         findWin = [[finder FinderWindows] objectWithID:[NSNumber numberWithInteger: findWin.id]];
@@ -47,30 +47,28 @@ int main(int argc, const char * argv[]) {
             target = [[findWin target] get];
             selected = false;
         }
-        
+
         if ([[target kind] isEqualToString:@"Alias"]){
             target = (FinderItem*)[(FinderAliasFile*)target originalItem];
         }
-        
+
         NSString* fileUrl = [target URL];
         if(fileUrl != nil && ![fileUrl hasSuffix:@"/"] && selected){
             fileUrl = [fileUrl stringByDeletingLastPathComponent];
         }
-        
+
         NSURL* url = [NSURL URLWithString:fileUrl];
-        
+
         if (![bundleID isEqualToString:@"com.apple.Terminal"]) {
-            SBApplication *app = [SBApplication applicationWithBundleIdentifier:bundleID];
-            NSTask *task = [NSTask new];
-            task.launchPath = @"/usr/bin/open";
-            task.arguments = @[@"-b", bundleID, url.path];
-            id err;
-            [task launchAndReturnError:&err];
-            if (!err) [task waitUntilExit];
+            [[NSWorkspace sharedWorkspace] openURLs:@[url]
+                                      withAppBundleIdentifier:bundleID
+                                                      options:NSWorkspaceLaunchDefault
+                               additionalEventParamDescriptor:nil
+                                            launchIdentifiers:nil];
         } else {
-            
+
             TerminalApplication* terminal = [SBApplication applicationWithBundleIdentifier:@"com.apple.Terminal"];
-            
+
             if (url != nil){
                 TerminalWindow* win = nil;
                 if ([[terminal windows] count] == 1){
@@ -83,7 +81,7 @@ int main(int argc, const char * argv[]) {
                 TerminalWindow* newWin = [[terminal windows] objectAtLocation:@1];
                 newWin = [[terminal windows] objectWithID: [NSNumber numberWithInteger:newWin.id]];
                 TerminalTab* newTab = [[newWin tabs] objectAtLocation:@1];
-                
+
                 NSString* setName = [[NSUserDefaults standardUserDefaults] stringForKey:@"cdto-new-window-setting"];
                 if(setName != nil && ![setName isEqualToString:@""]) { //setting set
                     TerminalSettingsSet* chosenSet = nil;
@@ -96,7 +94,7 @@ int main(int argc, const char * argv[]) {
                         newTab.currentSettings = chosenSet;
                     }
                 }
-                
+
                 if([[NSUserDefaults standardUserDefaults] boolForKey:@"cdto-close-default-window"]){ //close first launch window
                     if([[win tabs] count] == 1){
                         TerminalTab* tab = [[win tabs]objectAtLocation:@1];
@@ -114,8 +112,8 @@ int main(int argc, const char * argv[]) {
                         }
                     }
                 }
-                
-                
+
+
                 [terminal activate];
             }
         }
